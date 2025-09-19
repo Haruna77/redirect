7cafa5f0412d81944fff161139f828e55fd1e02b
Haruna77 <104721227+Haruna77@users.noreply.github.com>
2025-09-19 18:14:20 +0900

    Update kintone.js
    proでリライト

--- kintone.js
+++ kintone.js
@@ -1,14 +1,30 @@
 (function() {
-    "use strict";
-    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function(event) {
-      var record = event.record;
-      var purchaseItem = record['ルックアップ_購入商品']['value'];
-  
-      if (purchaseItem && purchaseItem.includes('卸')) {
-        record['wholesale_flag']['value'] = '卸';
+  'use strict';
+
+  // レコードの新規作成画面および編集画面で、保存実行前にイベントを発生させる
+  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function(event) {
+    const record = event.record;
+
+    // ルックアップフィールドの値を取得
+    const lookupValue = record['ルックアップ_購入商品'].value;
+
+    // 値の存在チェックと型チェック
+    // ルックアップフィールドの値がnullやundefined、または文字列型でない場合はエラーを回避する
+    if (lookupValue && typeof lookupValue === 'string') {
+      // ルックアップフィールドの値に「卸」が含まれているか判定
+      if (lookupValue.includes('卸')) {
+        // 含まれている場合、wholesale_flagフィールドに「卸」と入力
+        record['wholesale_flag'].value = '卸';
       } else {
-        record['wholesale_flag']['value'] = '';
+        // 含まれていない場合、wholesale_flagフィールドを空白にする
+        record['wholesale_flag'].value = '';
       }
-      return event;
-    });
-  })();
+    } else {
+      // ルックアップフィールドが空、または予期せぬ型の場合は、wholesale_flagフィールドを空白にする
+      record['wholesale_flag'].value = '';
+    }
+
+    // イベントオブジェクトを返して、レコードの保存処理を続行する
+    return event;
+  });
+})();
