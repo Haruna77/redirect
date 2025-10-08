9a242750f53c6ad583967a3896825e293d53e5c0
Haruna77 <104721227+Haruna77@users.noreply.github.com>
2025-10-08 17:26:46 +0900

    Update kintone.js
    住所チェックボックスをブランチから統合

--- kintone.js
+++ kintone.js
@@ -1,42 +1,70 @@
 (function() {
   'use strict';
 
-  // --- 設定値 ---
-  const lookupFieldCode = 'ルックアップ_購入商品';
-  const wholesaleFlagFieldCode = 'wholesale_flag'; // 卸フラグ用
-  const giftFlagFieldCode = 'gift_flag';      // Amazonギフト券フラグ用
-  // --- 設定値ここまで ---
+  // --- 機能1: 購入商品に応じたフラグ設定 ------------------------------------
+  (function() {
+    // --- 設定値 ---
+    const lookupFieldCode = 'ルックアップ_購入商品';
+    const wholesaleFlagFieldCode = 'wholesale_flag'; // 卸フラグ用
+    const giftFlagFieldCode = 'gift_flag';       // Amazonギフト券フラグ用
+    // --- 設定値ここまで ---
 
-  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function(event) {
-    const record = event.record;
+    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function(event) {
+      const record = event.record;
 
-    // ルックアップフィールドやその値が存在しない場合のエラーを防止する
-    if (!record[lookupFieldCode] || typeof record[lookupFieldCode].value === 'undefined') {
+      if (!record[lookupFieldCode] || typeof record[lookupFieldCode].value === 'undefined') {
+        return event;
+      }
+
+      const lookupValue = record[lookupFieldCode].value;
+      record[wholesaleFlagFieldCode].value = '';
+      record[giftFlagFieldCode].value = '';
+
+      if (lookupValue && typeof lookupValue === 'string') {
+        if (lookupValue.includes('卸')) {
+          record[wholesaleFlagFieldCode].value = '卸';
+        }
+        if (lookupValue.includes('Amazonギフト券あり')) {
+          record[giftFlagFieldCode].value = 'Amazonギフトあり';
+        }
+      }
       return event;
-    }
+    });
+  })();
 
-    // ルックアップフィールドの値（商品名）を取得
-    const lookupValue = record[lookupFieldCode].value;
 
-    // 各フラグを一旦リセット
-    record[wholesaleFlagFieldCode].value = '';
-    record[giftFlagFieldCode].value = '';
+  // --- 機能2: 住所コピー機能 ------------------------------------------
+  (function() {
+    // --- 設定値 ---
+    const checkboxFieldCode = 'チェックボックス_生徒住所と同じ';
+    const sourcePostalCode = '郵便番号_生徒住所';
+    const sourceName       = '名前_生徒住所';
+    const sourceAddress    = '住所_生徒住所_0';
+    const destPostalCode   = '郵便番号_郵送先';
+    const destName         = '宛名_郵送先';
+    const destAddress      = '住所_郵送先';
+    // --- 設定値ここまで ---
 
-    // 値の存在チェックと型チェック
-    if (lookupValue && typeof lookupValue === 'string') {
+    const events = [
+      'app.record.create.change.' + checkboxFieldCode,
+      'app.record.edit.change.' + checkboxFieldCode
+    ];
 
-      // 「卸」のチェック
-      if (lookupValue.includes('卸')) {
-        record[wholesaleFlagFieldCode].value = '卸';
+    kintone.events.on(events, function(event) {
+      const record = event.record;
+      const isChecked = record[checkboxFieldCode].value.length > 0;
+
+      if (isChecked) {
+        record[destPostalCode].value = record[sourcePostalCode].value;
+        record[destName].value       = record[sourceName].value;
+        record[destAddress].value    = record[sourceAddress].value;
+      } else {
+        record[destPostalCode].value = '';
+        record[destName].value       = '';
+        record[destAddress].value    = '';
       }
-
-      // 「Amazonギフト券」のチェック
-      if (lookupValue.includes('Amazonギフト券あり')) {
-        record[giftFlagFieldCode].value = 'Amazonギフトあり';
-      }
-    }
-    
-    return event;
-  });
+      return event;
+    });
+  })();
 
 })();
